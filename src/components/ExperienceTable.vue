aacdfbebcded8e8f3bcaae05a8c41cfe012a798a
Fletch Padgett <fletch@flatanimals.org>
2020-02-27 08:51:11 -0500

    make entire experience table row clickable

--- src/components/ExperienceTable.vue
+++ src/components/ExperienceTable.vue
@@ -2,36 +2,27 @@
   <table class="experience-table">
     <thead>
       <tr>
-        <th
-          class="experience-table-heading hidden sm:table-cell "
-        >Company</th>
-        <th
-          class="experience-table-heading"
-        >Position</th>
-        <th
-          class="experience-table-heading"
-        >From</th>
-        <th
-          class="experience-table-heading"
-        >To</th>
+        <th class="experience-table-heading hidden sm:table-cell">Company</th>
+        <th class="experience-table-heading">Position</th>
+        <th class="experience-table-heading">From</th>
+        <th class="experience-table-heading">To</th>
       </tr>
     </thead>
     <tbody>
       <tr
         v-for="exp in experiences"
         :key="exp.company"
-        :class="{'bg-yellow-200': hovered == exp.company}"
+        :class="{'bg-yellow-200': hovered == exp.company, 'cursor-pointer': true}"
+        @click="naivgateToResumeSection(exp.company)"
         @mouseover="setHover(exp.company)"
         @mouseout="clearHover"
       >
         <td class="px-2 py-2">
-          <router-link :to="'resume#' + exp.company.toLowerCase().replace(/\s/g,'-')">
-            <span class="font-normal">{{exp.company}}</span>
-            <span class="sm:hidden">
-              <br />
-              {{exp.title}}
-            </span>
-          </router-link>
+          <span class="font-normal">{{exp.company}}</span>
+          <span class="sm:hidden">
+            <br />
+            {{exp.title}}
+          </span>
         </td>
         <td class="hidden sm:table-cell px-2 py-2">{{exp.title}}</td>
         <td class="px-2 py-2">{{exp.from.getMonth() +1}}/{{exp.from.getFullYear()}}</td>
@@ -44,13 +35,16 @@
 <script>
 export default {
   name: "ExperienceTable",
-  props: ["experiences","hovered"],
+  props: ["experiences", "hovered"],
   methods: {
     setHover(company) {
-      this.$emit("setFocus", company)
+      this.$emit("setFocus", company);
     },
     clearHover() {
-      this.$emit("setFocus", "")
+      this.$emit("setFocus", "");
+    },
+    naivgateToResumeSection(company) {
+      this.$router.push('resume#' + company.toLowerCase().replace(/\s/g,'-'))
     }
   }
 };
@@ -59,18 +53,18 @@ export default {
 <style lang="postcss">
 .experience-table {
   @apply text-xs;
-  @apply  text-blue-900;
-  @apply  w-full;
-  @apply  font-light;
+  @apply text-blue-900;
+  @apply w-full;
+  @apply font-light;
 }
 .experience-table-heading {
   @apply px-2;
-  @apply  pt-1;
-  @apply  text-left;
-  @apply  border-b;
-  @apply  border-blue-500;
-  @apply  text-gray-500;
-  @apply  uppercase;
-  @apply  font-normal;
+  @apply pt-1;
+  @apply text-left;
+  @apply border-b;
+  @apply border-blue-500;
+  @apply text-gray-500;
+  @apply uppercase;
+  @apply font-normal;
 }
 </style>
